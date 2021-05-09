f02f4126f807c11687316f8c7a135ecde10056fb
mesteralfreddohnani <alfreddohnani@hotmail.com>
2021-05-09 21:32:43 +0000

    add production db url

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
 
+const dbUrl =
+  process.env.NODE_ENV === "production"
+    ? "mongodb+srv://mary:mary123@cluster0.gudcv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    : "mongodb://localhost:27017";
+
 mongoose
-    .connect("mongodb://localhost:27017", {
-        useCreateIndex: true,
-        useFindAndModify: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        dbName: "grace_fabrics"
-    })
-    .then(() => console.log("Database connection successful"))
-    .catch((err) => console.log(err.message));
+  .connect(dbUrl, {
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "grace_fabrics",
+  })
+  .then(() => console.log("Database connection successful"))
+  .catch((err) => console.log(err.message));
