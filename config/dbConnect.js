dc478cbb69f0a8244290f72f0c7b11f692754777
mesteralfreddohnani <alfreddohnani@hotmail.com>
2021-05-09 23:07:12 +0000

    fix: try 2 logging db connection

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 
-const dbUrl =
-  process.env.NODE_ENV === "production"
-    ? "mongodb+srv://mary:mary123@cluster0.gudcv.mongodb.net/grace_fabrics?retryWrites=true&w=majority"
-    : "mongodb://localhost:27017";
+const dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017";
 
 console.log(dbUrl);
 
